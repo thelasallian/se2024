6549755ee752e1e198243afc7e4001081da4b1a9
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-18 04:16:54 +0800

    feat: Add an alt to the TLS logo

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </head>
 <body class="bg-[url('./public/assets/img/background.png')]">
   <div id="content" class="max-w-[1440px] w-full flex items-center flex-col "> 
-    <img src="./public/assets/logos/tls-star-black.svg" class="mt-[5vh]" title="The LaSallian">
+    <img src="./public/assets/logos/tls-star-black.svg" class="mt-[5vh]" alt="The LaSallian" title="The LaSallian">
   </div>
 
   
