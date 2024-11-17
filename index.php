bb849f7dca975f966e9f375fad0079a5f7b52e70
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-18 03:27:47 +0800

    feat: Create index.php with boilerplate code

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="wclassth=device-wclassth, initial-scale=1.0">
+  <title>The LaSallian - At the seams</title>
+</head>
+<body>
+ 
+</body>
+</html>
