be9083cab3c96326dfe853ab70d51223bf2e4656
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2024-11-18 03:36:29 +0800

    feat: Add the TLS star white logo as the website icon

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="wclassth=device-wclassth, initial-scale=1.0">
   <title>The LaSallian - At the seams</title>
+  
+  <link rel="icon" type="image/x-icon" href="./public/assets/logos/tls-star-white.svg">
 </head>
 <body>
-  
+
 </body>
 </html>
